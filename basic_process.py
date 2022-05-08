5acc2d268caa27867aaed53aa17d7296ba6e46cc
Chen Tasker <chentasker1@gmail.com>
2022-05-08 20:12:21 +0300

    Small changes

--- basic_process.py
+++ basic_process.py
@@ -6,7 +6,6 @@ import numpy as np
 from matplotlib import pyplot as plt, cm
 from matplotlib.ticker import LinearLocator
 
-
 BUFFER_SIZE = 120
 
 
@@ -20,8 +19,10 @@ def basic_response_mid_row(output_dir: str, input_clip: str):
     clip_file_name = os.path.split(input_clip)[1]
     greyscale_frames = get_frames(input_clip)
     angle_response_over_time_array = get_emd_responses(greyscale_frames,
-                                                       PhotoreceptorImageConverter.PhotoreceptorImageConverter(PhotoreceptorImageConverter.make_gaussian_kernel(15), greyscale_frames[0].shape, 6000))
-    #pickle_output_array(angle_response_over_time_array, clip_file_name, output_dir)
+                                                       PhotoreceptorImageConverter.PhotoreceptorImageConverter(
+                                                           PhotoreceptorImageConverter.make_gaussian_kernel(15),
+                                                           greyscale_frames[0].shape, 6000))
+    # pickle_output_array(angle_response_over_time_array, clip_file_name, output_dir)
     save_surface_plot(angle_response_over_time_array, clip_file_name, output_dir)
 
 
@@ -32,14 +33,15 @@ def get_emd_responses(frames, photoreceptor: PhotoreceptorImageConverter.Photore
     :param photoreceptor: The converter of frames to photoreceptor responses.
     :return: The angle response array for each frame.
     """
-    angle_respone_over_time = []
+    angle_response_over_time = []
     emd = EMD.EMD()
     for buffer in photoreceptor.stream(frames, buffer_size=BUFFER_SIZE):
         emd_mid_row_response = emd.forward_row(buffer, buffer[0].shape[0] // 2)
         frequency_response_emd = [np.abs(np.fft.rfft(tr)) for tr in emd_mid_row_response]
         angle_response_emd = angle_response_from_frequency_response_array(frequency_response_emd)
-        angle_respone_over_time.append(angle_response_emd)
-    return np.array(angle_respone_over_time)
+        angle_response_over_time.append(angle_response_emd)
+    return np.array(angle_response_over_time)
+    #return np.array(emd_mid_row_response)
 
 
 def angle_response_from_frequency_response_array(frequency_response_array: np.array) -> np.array:
@@ -62,18 +64,19 @@ def angle_response_from_frequency_response_array(frequency_response_array: np.ar
     return angle_response_emd
 
 
-def get_frames(input_clip: str):
+def get_frames(input_clip: str, grayscale: bool = True):
     cap = cv2.VideoCapture(input_clip)
-    g_frames = []
+    frames = []
     while True:
         ret, frame = cap.read()
         if cv2.waitKey(1) & 0xFF == ord('q') or ret is False:
             cap.release()
             cv2.destroyAllWindows()
             break
-        g_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        g_frames.append(g_frame)
-    return g_frames
+        if grayscale:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        frames.append(frame)
+    return frames
 
 
 def save_surface_plot(output_array: np.array, clip_file_name: str, output_dir: str):
@@ -82,7 +85,8 @@ def save_surface_plot(output_array: np.array, clip_file_name: str, output_dir: s
     x = np.array(range(FRAMES))
     y = np.array(range(RESPONSES))
     X, Y = np.meshgrid(x, y)
-    surf = ax.plot_surface(np.transpose(X), np.transpose(Y), output_array, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+    surf = ax.plot_surface(np.transpose(X), np.transpose(Y), output_array, cmap=cm.coolwarm, linewidth=0,
+                           antialiased=False)
     ax.set_xlabel('time [frames]')
     ax.set_ylabel('EMD')
     ax.set_zlabel('Amplitude')
@@ -95,6 +99,7 @@ def save_surface_plot(output_array: np.array, clip_file_name: str, output_dir: s
     # Add a color bar which maps values to colors.
     fig.colorbar(surf, shrink=0.5, aspect=5)
     plt.savefig(os.path.join(output_dir, os.path.splitext(clip_file_name)[0] + '.png'))
+    plt.show()
     print("Done")
 
 
