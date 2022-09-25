7579410d9434bee17c78427f399befdfedc49372
mryuv1 <mryuv1@gmail.com>
2022-09-25 14:32:44 +0300

    The mnist_video is ready to use by 4 kernels

--- mnist/mnist_video.py
+++ mnist/mnist_video.py
@@ -6,26 +6,90 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
-
+import numpy as np
 from utils_for_DL import create_data_tuple
 
 
 class Net(nn.Module):
-    def __init__(self):
+    def __init__(self, **kwargs):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 16, 3, 3)  # format: (in_channels, out_channels, kermel_size, stride)
-        self.conv2 = nn.Conv2d(16, 32, 3, 3)
+        # args that we can change:
+        self.frame_dim = kwargs.setdefault('frame_dim', (4, 9, 220, 120))
+        self.kernel_size = kwargs.setdefault('kernel_size', 3)
+
+        # for conv3d layer 1:
+        self.stride1 = kwargs.setdefault('stride1', 1)
+        self.padding1 = kwargs.setdefault('padding1', 0)
+        self.dilation1 = kwargs.setdefault('dilation1', 1)
+        self.Cout1 = kwargs.setdefault('Cout1', 16)
+
+        # for conv3d layer 2:
+        self.stride2 = kwargs.setdefault('stride2', 1)
+        self.padding2 = kwargs.setdefault('padding2', 0)
+        self.dilation2 = kwargs.setdefault('dilation2', 1)
+        self.Cout2 = kwargs.setdefault('Cout2', 32)
+
+        # for maxpool 3D layer:
+        self.maxpool = kwargs.setdefault('maxpool', 2)
+        self.maxpool_stride = kwargs.setdefault('maxpool_stride', 1)
+
+        # for Linear layer 2:
+        self.linear2_input = kwargs.setdefault('linear2_input', 5280)
+
+        # -----------------------------------------------------------------------------
+        # calculating important dims for the network initiation:
+        # the format is (D, H, W) the letter is for the layer type (c for conv) and the index for the number:
+        self.d_c1 = torch.floor(
+            torch.tensor(((self.frame_dim[1] + 2 * self.padding1 - self.dilation1 * (
+                        self.kernel_size - 1) - 1) / self.stride1) + 1))
+
+        self.h_c1 = torch.floor(
+            torch.tensor(((self.frame_dim[2] + 2 * self.padding1 - self.dilation1 * (
+                        self.kernel_size - 1) - 1) / self.stride1) + 1))
+
+        self.w_c1 = torch.floor(
+            torch.tensor(((self.frame_dim[3] + 2 * self.padding1 - self.dilation1 * (
+                        self.kernel_size - 1) - 1) / self.stride1) + 1))
+
+        self.d_c2 = torch.floor(
+            ((self.d_c1 + 2 * self.padding1 - self.dilation1 * (self.kernel_size - 1) - 1) / self.stride1) + 1)
+
+        self.h_c2 = torch.floor(
+            ((self.h_c1 + 2 * self.padding1 - self.dilation1 * (self.kernel_size - 1) - 1) / self.stride1) + 1)
+
+        self.w_c2 = torch.floor(
+            ((self.w_c1 + 2 * self.padding1 - self.dilation1 * (self.kernel_size - 1) - 1) / self.stride1) + 1)
+
+
+        self.d_mp1 = torch.floor(
+            ((self.d_c2 - (self.maxpool - 1) - 1) / self.maxpool_stride) + 1)
+
+        self.h_mp1 = torch.floor(
+            ((self.h_c2 - (self.maxpool - 1) - 1) / self.maxpool_stride) + 1)
+
+        self.w_mp1 = torch.floor(
+            ((self.w_c2 - (self.maxpool - 1) - 1) / self.maxpool_stride) + 1)
+
+        self.linear1_input = int(self.Cout2 * self.d_mp1 * self.h_mp1 * self.w_mp1)
+
+        self.output_flatten_size = int(self.frame_dim[1] * self.frame_dim[2] * self.frame_dim[3])
+        # -----------------------------------------------------------------------------
+        # the network layers:
+        self.conv1 = nn.Conv3d(self.frame_dim[0], self.Cout1, self.kernel_size, stride=self.stride1, padding=self.padding1,
+                               dilation=self.dilation1)
+        self.conv2 = nn.Conv3d(self.Cout1, self.Cout2, self.kernel_size, stride=self.stride2, padding=self.padding2,
+                               dilation=self.dilation2)
         self.dropout1 = nn.Dropout(0.25)
         self.dropout2 = nn.Dropout(0.5)
-        self.fc1 = nn.Linear(2304, 5280)
-        self.fc2 = nn.Linear(5280, 26400)
+        self.fc1 = nn.Linear(self.linear1_input, self.linear2_input)
+        self.fc2 = nn.Linear( self.linear2_input, self.output_flatten_size)
 
     def forward(self, x):
         x = self.conv1(x)
         x = F.relu(x)
         x = self.conv2(x)
         x = F.relu(x)
-        x = F.max_pool2d(x, 2)
+        x = F.max_pool3d(x, self.maxpool, stride=self.maxpool_stride)
         x = self.dropout1(x)
         x = torch.flatten(x, 1)
         x = self.fc1(x)
@@ -38,11 +102,21 @@ class Net(nn.Module):
 
 def train(args, model, device, train_loader, optimizer, epoch, transform):
     model.train()
+    print('\033[95m' + '-----------------------------------------------------------------------------\n' + '\033[0m')
+    print(
+        '\033[96m' + f'This current option is for 2 kernels of Fourier (x,y) '
+                     f'and 2 in Glider.\nEach frame is ({check["frame_dim"][2]}, {check["frame_dim"][3]}).\n' + '\033[0m')
+    print('\033[95m' + '-----------------------------------------------------------------------------\n' + '\033[0m')
     for batch_idx, (data, target) in enumerate(list(train_loader.values())):
         # ----------------------------------------------------------------------------------------------------------#
-        # TODO: when we build the DATALOADER we need to delete the lines below
-        data, target = data[0], target[0]
-        data, target = transform(data), transform(target)
+        # TODO: when we build the DATALOADER we need to delete the lines below.
+        #       This current option is for 2 kernels of Fourier (x, y) and 2 in Glider
+        data, target = torch.tensor(np.array(data)), torch.tensor(np.array(target))
+        data, target = torch.unsqueeze(data, dim=0), torch.unsqueeze(target, dim=0)
+        data, target = data.repeat(4, 1, 1, 1), target
+
+        # tmp normalization:
+        data, target = data/255, target/255
         data, target = torch.unsqueeze(data, dim=0), torch.unsqueeze(target, dim=0)
         # ----------------------------------------------------------------------------------------------------------#
         data, target = data.to(device), target.to(device)
@@ -66,10 +140,14 @@ def test(model, device, test_loader, transform):
     correct = 0
     with torch.no_grad():
         for (data, target) in list(test_loader.values()):
-            data, target = data[0], target[0]
-            data, target = transform(data), transform(target)
+            data, target = torch.tensor(np.array(data)), torch.tensor(np.array(target))
             data, target = torch.unsqueeze(data, dim=0), torch.unsqueeze(target, dim=0)
-            data, target = data.to(device), target.to(device)
+            data, target = data.repeat(4, 1, 1, 1), target
+
+            # tmp normalization:
+            data, target = data / 255, target / 255
+            data, target = torch.unsqueeze(data, dim=0), torch.unsqueeze(target, dim=0)
+
             output = model(data)
             loss = nn.MSELoss()
             loss = loss(output, torch.flatten(target, 1))
@@ -80,8 +158,7 @@ def test(model, device, test_loader, transform):
     print(f'The test loss is: {test_loss}')
 
 
-
-def main():
+def main(model_in_parameters=dict()):
     # Training settings
     parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
     parser.add_argument('--batch-size', type=int, default=64, metavar='N',
@@ -125,7 +202,7 @@ def main():
         transforms.Normalize((0.1307,), (0.3081,))
     ])
 
-    model = Net().to(device)
+    model = Net(**model_in_parameters).to(device)
     optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
 
     scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
@@ -141,17 +218,23 @@ def main():
 
 if __name__ == '__main__':
     import os
+
     # If you want to run it you need to extract the dataset from the zip
     general_DS_folser = os.path.join('D:\Data_Sets', 'DAVIS-2017-trainval-480p', 'DAVIS')
 
+    desiered_dim = (40, 40)
+
     # The dataset format is:
     # {'name_of_video', raw jpegs, segmented data}
-    dataset_dict = create_data_tuple(general_DS_folser, number_of_videos=40, desiered_dim=(220, 120))
-    main()
+    dataset_dict = create_data_tuple(general_DS_folser, number_of_videos=40, desiered_dim=desiered_dim,
+                                     number_of_frames=9)
+    check = {'frame_dim': (4, 9, desiered_dim[0], desiered_dim[1]), 'Cout2': 16}
+
+    main(check)
 
     """
     Things that we need to talk about:
-    1. what is the resolution that we put in the net.
-    2. how many channels.
-    3. should we start with a segmentaion of a single frame or to start directly with couple of frames.
+    1. what is the resolution that we put in the net
+    2. how many channels -option1->  F:x,y ; G:x,y  -option2->  option1 + other_2*x,y
+    3. should we start with a segmentaion of a single frame or to start directly with couple of frames. - Answer: video
     """
--- mnist/utils_for_DL.py
+++ mnist/utils_for_DL.py
@@ -1,24 +1,28 @@
 import cv2
 import os
 import matplotlib.pyplot as plt
+import numpy as np
 
 
-
-def read_all_pictures_in_file(dir, desiered_dim=None) -> list:
+def read_all_pictures_in_file(dir, desiered_dim=None, number_of_frames=np.inf) -> list:
     final_list = list()
     pics_names = os.listdir(dir)
+    loop_idx = 0
     for pic in pics_names:
+        loop_idx += 1
         pic_tmp = cv2.imread(os.path.join(dir, pic))
         gray_tmp = cv2.cvtColor(pic_tmp, cv2.COLOR_BGR2GRAY)
 
         if desiered_dim:
             gray_tmp = cv2.resize(gray_tmp, desiered_dim)
         final_list.append(gray_tmp)
+        if loop_idx == number_of_frames:
+            break
 
     return final_list
 
 
-def create_data_tuple(dir, number_of_videos=5, desiered_dim=None) -> dict:
+def create_data_tuple(dir, number_of_videos=5, desiered_dim=None, number_of_frames=np.inf) -> dict:
     annotations_path = os.path.join(dir, 'Annotations', '480p')
     JPEG_path = os.path.join(dir, 'JPEGImages', '480p')
     anotations_list = os.listdir(annotations_path)
@@ -27,40 +31,41 @@ def create_data_tuple(dir, number_of_videos=5, desiered_dim=None) -> dict:
         annototion_vid_path = os.path.join(annotations_path, anotations_list[video_idx])
         JPEG_path_vid_path = os.path.join(JPEG_path, anotations_list[video_idx])
 
-        annotation_list = read_all_pictures_in_file(annototion_vid_path, desiered_dim=desiered_dim)
-        JPEG_list = read_all_pictures_in_file(JPEG_path_vid_path, desiered_dim=desiered_dim)
+        annotation_list = read_all_pictures_in_file(annototion_vid_path, desiered_dim=desiered_dim,
+                                                    number_of_frames=number_of_frames)
+        JPEG_list = read_all_pictures_in_file(JPEG_path_vid_path, desiered_dim=desiered_dim,
+                                              number_of_frames=number_of_frames)
 
         dataset_dict[anotations_list[video_idx]] = (JPEG_list, annotation_list)
     return dataset_dict
 
 
-
 if __name__ == '__main__':
     general_DS_folser = os.path.join('D:\Data_Sets', 'DAVIS-2017-trainval-480p', 'DAVIS')
 
-    dataset_dict = create_data_tuple(general_DS_folser, desiered_dim=(800, 500))
+    dataset_dict = create_data_tuple(general_DS_folser, desiered_dim=(200, 100))
     print(dataset_dict.keys())
 
     plt.imshow(dataset_dict['bear'][0][2], cmap='gray')
-    new_dim = (dataset_dict['bear'][0][2].shape[1]//4, dataset_dict['bear'][0][2].shape[0]//4)
+    new_dim = (dataset_dict['bear'][0][2].shape[1] // 4, dataset_dict['bear'][0][2].shape[0] // 4)
     resized = cv2.resize(dataset_dict['bear'][0][2], new_dim)
     plt.imshow(resized, cmap='gray')
     plt.show()
 
     plt.imshow(dataset_dict['bear'][1][2], cmap='gray')
-    new_dim = (dataset_dict['bear'][1][2].shape[1]//4, dataset_dict['bear'][0][2].shape[0]//4)
+    new_dim = (dataset_dict['bear'][1][2].shape[1] // 4, dataset_dict['bear'][0][2].shape[0] // 4)
     resized = cv2.resize(dataset_dict['bear'][1][2], new_dim)
     plt.imshow(resized, cmap='gray')
     plt.show()
 
     plt.imshow(dataset_dict['bmx-trees'][0][2], cmap='gray')
-    new_dim = (dataset_dict['bmx-trees'][0][2].shape[1]//4, dataset_dict['bmx-trees'][0][2].shape[0]//4)
+    new_dim = (dataset_dict['bmx-trees'][0][2].shape[1] // 4, dataset_dict['bmx-trees'][0][2].shape[0] // 4)
     resized = cv2.resize(dataset_dict['bmx-trees'][0][2], new_dim)
     plt.imshow(resized, cmap='gray')
     plt.show()
 
     plt.imshow(dataset_dict['bmx-trees'][1][2], cmap='gray')
-    new_dim = (dataset_dict['bmx-trees'][1][2].shape[1]//4, dataset_dict['bmx-trees'][0][2].shape[0]//4)
+    new_dim = (dataset_dict['bmx-trees'][1][2].shape[1] // 4, dataset_dict['bmx-trees'][0][2].shape[0] // 4)
     resized = cv2.resize(dataset_dict['bmx-trees'][1][2], new_dim)
     plt.imshow(resized, cmap='gray')
     plt.show()
