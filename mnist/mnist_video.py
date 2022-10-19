cacc798e4b79c6a7b6941c9c74640bfdc001ab77
mryuv1 <mryuv1@gmail.com>
2022-10-19 12:26:29 +0300

    check 3

--- mnist/mnist_video.py
+++ mnist/mnist_video.py
@@ -1,5 +1,7 @@
 from __future__ import print_function
 import argparse
+import sys
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -8,6 +10,33 @@ from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
 import numpy as np
 from utils_for_DL import create_data_tuple
+from torch.utils.data import Dataset
+from torch.utils.tensorboard import SummaryWriter
+
+
+# setup the writer
+writer = SummaryWriter('runs')
+
+
+class CustomImageDataset(Dataset):
+    def __init__(self, dataset_dict, transform=None, target_transform=None):
+        self.dataset_dict = dataset_dict
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        return len(self.dataset_dict)
+
+    def __getitem__(self, idx):
+        names = list(self.dataset_dict.keys())
+        name_idx = names[idx]
+        images = self.dataset_dict[name_idx][0]
+        segmentation = self.dataset_dict[name_idx][1]
+        if self.transform:
+            images = self.transform(images)
+        if self.target_transform:
+            segmentation = self.target_transform(segmentation)
+        return images, segmentation
 
 
 class Net(nn.Module):
@@ -41,15 +70,15 @@ class Net(nn.Module):
         # the format is (D, H, W) the letter is for the layer type (c for conv) and the index for the number:
         self.d_c1 = torch.floor(
             torch.tensor(((self.frame_dim[1] + 2 * self.padding1 - self.dilation1 * (
-                        self.kernel_size - 1) - 1) / self.stride1) + 1))
+                    self.kernel_size - 1) - 1) / self.stride1) + 1))
 
         self.h_c1 = torch.floor(
             torch.tensor(((self.frame_dim[2] + 2 * self.padding1 - self.dilation1 * (
-                        self.kernel_size - 1) - 1) / self.stride1) + 1))
+                    self.kernel_size - 1) - 1) / self.stride1) + 1))
 
         self.w_c1 = torch.floor(
             torch.tensor(((self.frame_dim[3] + 2 * self.padding1 - self.dilation1 * (
-                        self.kernel_size - 1) - 1) / self.stride1) + 1))
+                    self.kernel_size - 1) - 1) / self.stride1) + 1))
 
         self.d_c2 = torch.floor(
             ((self.d_c1 + 2 * self.padding1 - self.dilation1 * (self.kernel_size - 1) - 1) / self.stride1) + 1)
@@ -59,7 +88,6 @@ class Net(nn.Module):
 
         self.w_c2 = torch.floor(
             ((self.w_c1 + 2 * self.padding1 - self.dilation1 * (self.kernel_size - 1) - 1) / self.stride1) + 1)
-
 
         self.d_mp1 = torch.floor(
             ((self.d_c2 - (self.maxpool - 1) - 1) / self.maxpool_stride) + 1)
@@ -75,14 +103,15 @@ class Net(nn.Module):
         self.output_flatten_size = int(self.frame_dim[1] * self.frame_dim[2] * self.frame_dim[3])
         # -----------------------------------------------------------------------------
         # the network layers:
-        self.conv1 = nn.Conv3d(self.frame_dim[0], self.Cout1, self.kernel_size, stride=self.stride1, padding=self.padding1,
+        self.conv1 = nn.Conv3d(self.frame_dim[0], self.Cout1, self.kernel_size, stride=self.stride1,
+                               padding=self.padding1,
                                dilation=self.dilation1)
         self.conv2 = nn.Conv3d(self.Cout1, self.Cout2, self.kernel_size, stride=self.stride2, padding=self.padding2,
                                dilation=self.dilation2)
         self.dropout1 = nn.Dropout(0.25)
         self.dropout2 = nn.Dropout(0.5)
         self.fc1 = nn.Linear(self.linear1_input, self.linear2_input)
-        self.fc2 = nn.Linear( self.linear2_input, self.output_flatten_size)
+        self.fc2 = nn.Linear(self.linear2_input, self.output_flatten_size)
 
     def forward(self, x):
         x = self.conv1(x)
@@ -107,6 +136,7 @@ def train(args, model, device, train_loader, optimizer, epoch, transform):
         '\033[96m' + f'This current option is for 2 kernels of Fourier (x,y) '
                      f'and 2 in Glider.\nEach frame is ({check["frame_dim"][2]}, {check["frame_dim"][3]}).\n' + '\033[0m')
     print('\033[95m' + '-----------------------------------------------------------------------------\n' + '\033[0m')
+    running_loss = 0.0
     for batch_idx, (data, target) in enumerate(list(train_loader.values())):
         # ----------------------------------------------------------------------------------------------------------#
         # TODO: when we build the DATALOADER we need to delete the lines below.
@@ -116,7 +146,7 @@ def train(args, model, device, train_loader, optimizer, epoch, transform):
         data, target = data.repeat(4, 1, 1, 1), target
 
         # tmp normalization:
-        data, target = data/255, target/255
+        data, target = data / 255, target / 255
         data, target = torch.unsqueeze(data, dim=0), torch.unsqueeze(target, dim=0)
         # ----------------------------------------------------------------------------------------------------------#
         data, target = data.to(device), target.to(device)
@@ -126,10 +156,14 @@ def train(args, model, device, train_loader, optimizer, epoch, transform):
         loss = loss(output, torch.flatten(target, 1))
         loss.backward()
         optimizer.step()
+        running_loss += loss.item()
         if batch_idx % args.log_interval == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader),
                        100. * batch_idx / len(train_loader), loss.item()))
+            writer.add_scalar('training loss', loss.item() / args.log_interval,
+                              epoch * len(list(train_loader.values())) + batch_idx)
+
             if args.dry_run:
                 break
 
@@ -184,6 +218,7 @@ def main(model_in_parameters=dict()):
     args = parser.parse_args()
     use_cuda = not args.no_cuda and torch.cuda.is_available()
 
+
     torch.manual_seed(args.seed)
 
     device = torch.device("cuda" if use_cuda else "cpu")
@@ -205,10 +240,22 @@ def main(model_in_parameters=dict()):
     model = Net(**model_in_parameters).to(device)
     optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
 
+    data, target = list(dataset_dict.values())[0]
+    data, target = torch.tensor(np.array(data)), torch.tensor(np.array(target))
+    data, target = torch.unsqueeze(data, dim=0), torch.unsqueeze(target, dim=0)
+    data, target = data.repeat(4, 1, 1, 1), target
+
+    # tmp normalization:
+    data, target = data / 255, target / 255
+    data, target = torch.unsqueeze(data, dim=0), torch.unsqueeze(target, dim=0)
+
+    writer.add_graph(model, data)
+
     scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
     for epoch in range(1, args.epochs + 1):
         train(args, model, device, dataset_dict, optimizer, epoch, transform=transform)  # dataset_dict
-
+        writer.close()
+        sys.exit()
         test(model, device, dataset_dict, transform=transform)
         scheduler.step()
 
@@ -218,6 +265,10 @@ def main(model_in_parameters=dict()):
 
 if __name__ == '__main__':
     import os
+
+    # import wandb
+    #
+    # wandb.init(project="test-project", entity="yuvalandchen")
 
     # If you want to run it you need to extract the dataset from the zip
     general_DS_folser = os.path.join('D:\Data_Sets', 'DAVIS-2017-trainval-480p', 'DAVIS')
@@ -229,7 +280,10 @@ if __name__ == '__main__':
     dataset_dict = create_data_tuple(general_DS_folser, number_of_videos=40, desiered_dim=desiered_dim,
                                      number_of_frames=9)
     check = {'frame_dim': (4, 9, desiered_dim[0], desiered_dim[1]), 'Cout2': 16}
-    # for the commit
+
+    # TODO: this is the start of data loader, still we need to built all of it's attributes
+    loader = CustomImageDataset(dataset_dict)
+
     main(check)
 
     """
